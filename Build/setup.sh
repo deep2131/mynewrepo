a04900ed39dcc7c45686c4273d73c566f51037a4
deep2131 <58816012+deep2131@users.noreply.github.com>
2019-12-17 17:43:59 +0530

    Update setup.sh

--- Build/setup.sh
+++ Build/setup.sh
@@ -1,7 +1,10 @@
 
 #!/bin/bash
-ls 
 pwd
-tar xvf buildartifact.tar -C /home/ec2-user/
+ls -ltr /home/ec2-user
+
+tar xvf /home/ec2-user/buildartifact.tar 
+echo "printing now...."
+ls -ltr /home/ec2-user
 
 
